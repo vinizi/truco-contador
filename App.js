8fc3506b71b18e8b85419acf9c574b4f561930f2
Vinícius <vinicius121103@edu.unipar.br>
2026-03-16 20:54:02 -0300

    feat: adiciona contador simples com incremento e decrement o

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
+
+  const [pontos, setPontos] = useState(0);
+
+  function diminuir() {
+    if (pontos > 0) {
+      setPontos(pontos - 1);
+    }
+  }
+  function limite(){
+    if (pontos < 12){ 
+      setPontos(pontos + 1)
+   }else{
+    setPontos(0)
+   }
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+
+      <Image
+        source={{ uri: "https://brandigno.com.br/wp-content/uploads/2017/04/thumb-unipar.png" }}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+
+      <Text style={styles.titulo}>MARCADOR</Text>
+
+      <Text style={styles.pontuacao}>{pontos}</Text>
+
+      <View style={styles.botoes}>
+        <TouchableOpacity
+          style={styles.botaoMais}
+          onPress={limite}
+        >
+          <Text style={styles.textoBotao}>+</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.botaoMenos}
+          onPress={diminuir}
+        >
+          <Text style={styles.textoBotao}>-</Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +59,51 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+
+    padding: 20,
+  },
+
+  logo: {
+    width: 400,    
+    height: 300,   
+    marginBottom: 0,
+  },
+
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+
+  pontuacao: {
+    fontSize: 70,
+    fontWeight: 'bold',
+    marginVertical: 60,
+  },
+
+  botoes: {
+    flexDirection: 'row',
+  },
+
+  botaoMais: {
+    backgroundColor: '#0B6B3A',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    marginRight: 10, 
+  },
+
+  botaoMenos: {
+    backgroundColor: '#8B0015',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+  },
+
+  textoBotao: {
+    color: 'white',
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
